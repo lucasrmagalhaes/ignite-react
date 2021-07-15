3a4e9357554160274923e148fe4962bc0f86fce7
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-15 17:48:08 -0300

    Carregando transações

--- 02-dt-money/src/App.tsx
+++ 02-dt-money/src/App.tsx
@@ -4,7 +4,7 @@ import Modal from 'react-modal'
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -20,7 +20,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -31,6 +31,6 @@ export function App() {
       />
       
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
--- 02-dt-money/src/components/Summary/index.tsx
+++ 02-dt-money/src/components/Summary/index.tsx
@@ -1,14 +1,14 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext'
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from "./styles";
 
 export function Summary() {
-  const data = useContext(TransactionsContext);
-  console.log(data)
+  const transactions = useContext(TransactionsContext)
+  console.log(transactions)
 
   return (
     <Container>
