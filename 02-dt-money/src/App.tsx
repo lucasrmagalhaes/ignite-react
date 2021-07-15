578841ca76303562d5f71da0c0ab0e263d9f4c8f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-15 13:11:00 -0300

    Componente: NewTransactionModal

--- 02-dt-money/src/App.tsx
+++ 02-dt-money/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal'
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -16,17 +17,18 @@ export function App() {
   function handleCloseNewTransactionModal() {
     setNewTransactionModalOpen(false)
   }
-  
+
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
+      
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      />
+      
       <GlobalStyle />
     </>
   );
